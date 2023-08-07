82028f1b70723c8cef89c4bae28f4302932b51f6
Orifjon <orifjon.olimjonov@mail.ru>
2023-08-07 14:31:34 +0500

    Color Picker has been organized.

--- app/src/main/java/uz/orifjon/noteapp/presentation/addfragment/AddFragment.kt
+++ app/src/main/java/uz/orifjon/noteapp/presentation/addfragment/AddFragment.kt
@@ -1,5 +1,6 @@
 package uz.orifjon.noteapp.presentation.addfragment
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,14 +11,16 @@ import uz.orifjon.noteapp.databinding.FragmentAddBinding
 
 class AddFragment : Fragment() {
 
-    private var _binding: FragmentAddBinding?=null
+    private var _binding: FragmentAddBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentAddBinding.inflate(inflater,container,false)
+        _binding = FragmentAddBinding.inflate(inflater, container, false)
+
+
 
 
         return binding.root
